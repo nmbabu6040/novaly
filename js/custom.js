1315aa36a20967b28e0262609a9fecb5cf51b032
N M Babu <nmbabu6040@gmail.com>
2025-07-09 20:50:19 +0600

    novaly
    novaly

---
+++ js/custom.js
@@ -0,0 +1,140 @@
+
+//banner slider
+$('.banner_slider').owlCarousel({
+    loop:true,
+    dots: false,
+    animateOut: 'fadeOut',
+    autoplay:true,
+    smartSpeed: 500,
+    nav: true,
+    navText: ['<i class="fa-solid fa-caret-left leftside"></i>','<i class="fa-solid fa-caret-right rightside"></i>'],
+    items:1,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            
+        },
+        600:{
+            items:1,
+            
+        },
+        1000:{
+            items:1,
+            
+        }
+    }
+})
+
+// counter js start 
+$(function(){
+  $('.counter').rCounter({
+    duration: 30
+  });
+});
+
+//venubox
+new VenoBox({
+    selector: '.my-video-links',
+});
+
+//consulting slider
+$('.consulting_slider').owlCarousel({
+    loop:true,
+    dots: false,
+    margin: 20,
+    //animateOut: 'fadeOut',
+    smartSpeed: 500,
+    nav: true,
+    navText: ['<i class="fa-solid fa-chevron-left leftside"></i>','<i class="fa-solid fa-chevron-right rightside"></i>'],
+    items:3,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            
+        },
+        576:{
+            items:2,
+            
+        },
+        768:{
+            items:3,
+            
+        },
+        1000:{
+            items:3,
+            
+        },
+        1200:{
+            items:3,
+        }
+    }
+})
+
+//client slider
+$('.client_slider').owlCarousel({
+    loop:true,
+    dots: false,
+    margin: 20,
+    autoplay: true,
+    autoplayHoverPause: true,
+    //animateOut: 'fadeOut',
+    smartSpeed: 500,
+    nav: false,
+    navText: ['<i class="fa-solid fa-chevron-left leftside"></i>','<i class="fa-solid fa-chevron-right rightside"></i>'],
+    items:4,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            
+        },
+        576:{
+            items:2,
+            
+        },
+        768:{
+            items:3,
+            
+        },
+        1000:{
+            items:3,
+            
+        },
+        1200:{
+            items:4,
+        }
+    }
+})
+
+//test slider
+$('.testimonial_slider').owlCarousel({
+    loop:true,
+    dots: false,
+    margin: 20,
+    smartSpeed: 500,
+    nav: true,
+    navText: ['<i class="fa-solid fa-chevron-left leftside"></i>','<i class="fa-solid fa-chevron-right rightside"></i>'],
+    items:2,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            
+        },
+        600:{
+            items:1,
+            
+        },
+        1000:{
+            items:2,
+            
+        },
+        1200:{
+            items:2,
+        }
+    }
+})
+//AOS
+AOS.init();
